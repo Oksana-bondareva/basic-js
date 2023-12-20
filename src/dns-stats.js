158f21dedcb1ac7c13bad04c28439552ae7c3c0b
Oksana-bondareva <oksanchik252@gmail.com>
2023-12-21 00:14:21 +0300

    feat: add solution task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  let reversArray = [];
+  domains.forEach(e => {
+    reversArray.push(e.split('.').reverse());
+  });
+  reversArray.forEach(e => {
+    let domain ='';
+    for (let i = 0; i < e.length; i++) {
+      domain += '.' + e[i];
+      if (!result[domain]) {
+        result[domain] = 1;
+      } else {
+        result[domain]++;
+      }
+    };
+  })
+  return result;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 1;
+    for (let i = 0; i <= arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+          let el = this.calculateDepth(arr[i]) + 1;
+          count = Math.max(count, el);
+      }
+    }
+    return count;
   }
 }
 
