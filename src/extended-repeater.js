e91145b878d2689646bc8dcd7f7e92541b02687e
Oksana-bondareva <oksanchik252@gmail.com>
2023-12-22 00:46:07 +0300

    feat: add solution task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const {
+    repeatTimes = 1,
+		separator = '+',
+		addition = '',
+		additionRepeatTimes = 1,
+		additionSeparator = '|',
+  } = options;
+
+  let resultString = Array(additionRepeatTimes).fill(String(addition)).join(additionSeparator);
+  return Array(repeatTimes).fill(String(str) + resultString).join(separator);
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let result = arr.filter((el) => el != -1).sort((a,b) => a-b);
+  arr.map((n, index) => {
+    if(n == -1) {
+      result.splice(index, 0, n);
+    }
+  });
+  return result;
 }
 
 module.exports = {
